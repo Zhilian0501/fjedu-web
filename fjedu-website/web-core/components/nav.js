98eceb4e68fd950cfdb5ea0013b08210afbfc5ef
yeh0315 <91412327+yeh0315@users.noreply.github.com>
2025-06-03 19:37:04 +0000

    Changes to be committed: 	modified:   fjedu-website/web-core/components/nav.js

--- fjedu-website/web-core/components/nav.js
+++ fjedu-website/web-core/components/nav.js
@@ -1,3 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const mainNav = document.querySelector('.main-nav');
+
+hamburger.addEventListener('click', () => {
+  mainNav.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".main-nav").classList.toggle("active");
