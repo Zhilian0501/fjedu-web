d6e776ac16e34c49e031fe3234034a402ccb3f6a
yeh0315 <91412327+yeh0315@users.noreply.github.com>
2025-06-12 16:30:30 +0000

    Changes to be committed: 	modified:   fjedu-website/web-core/server.js

--- fjedu-website/web-core/server.js
+++ fjedu-website/web-core/server.js
@@ -16,6 +16,22 @@ app.use(express.urlencoded({ extended: false }));
 
 const allowedOrigins = ['https://fjedu-web.pages.dev', 'https://fjedu.online'];
 
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  preflightContinue: false
+};
+
+app.use(cors(corsOptions));
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin) return callback(null, true);
