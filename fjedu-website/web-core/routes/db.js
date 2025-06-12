ef0697e0957e3acf834df5d5d7b8434f5326fd5d
yeh0315 <91412327+yeh0315@users.noreply.github.com>
2025-06-12 14:22:07 +0000

    Changes to be committed: 	modified:   fjedu-website/web-core/routes/db.js

--- fjedu-website/web-core/routes/db.js
+++ fjedu-website/web-core/routes/db.js
@@ -2,12 +2,15 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const db = await mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default pool;
