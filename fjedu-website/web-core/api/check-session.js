d1279a2d75e36e34e99310672d18a45bc9581e4e
Zhilian0501 <zhilianshuwei@gmail.com>
2025-06-15 05:02:25 +0000

    Changes to be committed: 	modified:   fjedu-website/web-core/api/check-session.js 	modified:   fjedu-website/web-core/login.html

--- fjedu-website/web-core/api/check-session.js
+++ fjedu-website/web-core/api/check-session.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 // 檢查登入狀態
+console.log('收到 /check-session 請求，cookie:', req.headers.cookie);
 router.get('/check-session', (req, res) => {
   if (req.session && req.session.user) {
     res.json({ username: req.session.user.username });
