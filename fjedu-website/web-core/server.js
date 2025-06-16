611beff0fab7ae16d1393dc887d0079df2f7e7d6
Zhilian0501 <zhilianshuwei@gmail.com>
2025-06-16 08:29:45 +0000

    Changes to be committed: 	modified:   fjedu-website/web-core/server.js

--- fjedu-website/web-core/server.js
+++ fjedu-website/web-core/server.js
@@ -21,13 +21,13 @@ app.use(cors({
   credentials: true,
 }));
 
-//app.options('*', (req, res) => {
-// res.header('Access-Control-Allow-Origin', 'https://fjedu.online');
-//  res.header('Access-Control-Allow-Credentials', 'true');
-//  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//  res.header('Access-Control-Allow-Headers', 'Content-Type');
-//  res.sendStatus(204);
-//});
+app.options('*', (req, res) => {
+ res.header('Access-Control-Allow-Origin', 'https://fjedu.online');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.sendStatus(204);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
